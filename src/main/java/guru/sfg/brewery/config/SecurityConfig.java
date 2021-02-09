94d51e60de7a1dfc45620ee4cfe79b4f002cc41b
quipcode <abdulkarim8ahmed@gmail.com>
2021-02-09 12:28:46 -0800

    1-6

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,10 +1,23 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,4 +39,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().and()
                 .httpBasic();
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//        return new LdapShaPasswordEncoder();
+//        return new StandardPasswordEncoder();
+//        return new BCryptPasswordEncoder();
+        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("spring")
+                .password("{bcrypt}$2a$10$qLzjuHiYnmhejnlOgJV3dOBv1zwLXVGPwyi2jjxSAOEkKkPMJNpna")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{sha256}53e775451903393fcfba3af1808046a21f670df9262c83b59ca34904bb2ba5ec87fad1aa3aca9329")
+                .roles("USER")
+                .and()
+                .withUser("scott")
+                .password("{ldap}{SSHA}9yr9ceXrqWriJ7kDeHyOLiPBQsJbLGpaZGmt9g==")
+                .roles("CUSTOMER");
+    }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("spring")
+//                .password("guru")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+
 }
